63703c7f67061daf1e6a87dc1973df1115098c3d
samuraipapa <yury.gitman@gmail.com>
2015-03-06 10:46:56 -0500

    Initial Commit

---
+++ CoreBluetoothBasicScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreBluetoothBasicScanner
+//
+//  Created by GrownYoda on 3/6/15.
+//  Copyright (c) 2015 yuryg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
